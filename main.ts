3e6e3d4ebd13571adfb9e088df84ad72f4c83662
supercarsaresick <122555323+supercarsaresick@users.noreply.github.com>
2023-04-18 12:17:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,9 +6,11 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         . . # . .
         `)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, 100)
     wuKong.setMotorSpeed(wuKong.MotorList.M2, 100)
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
+    wuKong.stopMotor(wuKong.MotorList.M1)
     wuKong.stopMotor(wuKong.MotorList.M2)
 })
